0e17c302f5607a4bbc4b8e007b0f1a978c434684
EsmeraldaMTZV <23308060610235@cetis61.edu.mx>
2026-03-12 11:12:47 -0600

    act

---
+++ main.py
@@ -0,0 +1,43 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Inicio de sesion"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    sesion = ft.Container(
+        width=350,
+        padding=30,
+        border_radius=10,
+        bgcolor="white",
+        content=ft.Column(
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            spacing=15,
+            controls=[
+                ft.Text("Iniciar sesión", size=24, weight="bold"),
+
+                ft.TextField(
+                    label="Correo electrónico",
+                    width=280,
+                ),
+
+                ft.TextField(
+                    label="Introduzca su contraseña",
+                    password=True,
+                    can_reveal_password=True,
+                    width=280
+                ),
+
+                ft.Button(
+                    content="Entrar",
+                    width=280,
+                    bgcolor="#FFC0CB",
+                    
+                ),
+            ]
+        )
+    )
+
+    page.add(sesion)
+
+ft.app(target=main)
